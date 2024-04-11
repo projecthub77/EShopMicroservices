8a48fb1acafc54f83b3bb70dd15e677568207fc4
Developer <Developer@DESKTOP-06TNH19>
2024-04-11 17:51:01 +0200

    gestione presentation layer (UI) endpoint product

---
+++ src/BuildingBlocks/BuildingBlocks/CQRS/ICommandHandler.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingBlocks.CQRS
+{
+    //-------------------------------- Gestori centralizzati -----------------------------------------------------
+
+    public interface ICommandHandler<in TCommand>  // gestore del comando per risposte nulle (Unit)
+        : ICommandHandler<TCommand, Unit> 
+        where TCommand : ICommand<Unit>
+    {
+
+    }
+
+    public interface ICommandHandler<in TCommand, TResponse> // gestore del comando per ricevere risposte
+        : IRequestHandler<TCommand, TResponse>
+        where TCommand : ICommand<TResponse> 
+        where TResponse : notnull
+    {
+
+    }
+}
--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,6 +1,30 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public class CreateProductHandler
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = command.Name,
+                Category = command.Category,
+                Description = command.Description,
+                ImageFile = command.ImageFile,
+                Price = command.Price
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
+
+            //throw new NotImplementedException();
+        }
     }
 }
