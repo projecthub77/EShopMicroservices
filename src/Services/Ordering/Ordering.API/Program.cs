7b9e4d8b27099cf5b31e97b919c9a4600b9a268c
Developer <Developer@DESKTOP-06TNH19>
2024-05-16 15:38:56 +0200

    inizio DDD domain

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,5 +1,12 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddApplicationServices()
+                .AddInfrastructureServices(builder.Configuration)
+                .AddApiServices(builder.Configuration);
 
 var app = builder.Build();
 
