51a6e15d6c5344fc0b505e16e7f1a31fba2ededc
Developer <Developer@DESKTOP-06TNH19>
2024-05-17 17:59:58 +0200

    domain ddd start

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _domainEvents = new();
+        public IReadOnlyList<IDomainEvent> DomainEvents => throw new NotImplementedException();
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
